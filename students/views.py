0b04429cfcef89ee20100a96e4aeef9f072a7191
inv-najil <najil.arfak@innovaturelabs.com>
2025-07-14 09:54:48 +0530

    feat:added feature to add student details from csv

--- students/views.py
+++ students/views.py
@@ -5,8 +5,10 @@ from .models import Student
 from .serializers import StudentSerializer
 from students.permissions import IsAdminOrTeacherOnly
 from teachers.models import Teacher
+from accounts.serializers import RegistrationSerializer
 from django.http import HttpResponse
 import csv
+from io import TextIOWrapper
 
 """
 Student curd opersations with permission class and quetset modified to see only their data
@@ -88,3 +90,60 @@ class StudentView(viewsets.ModelViewSet):
                 student.assigned_teacher.user.username if student.assigned_teacher else ''
             ])
         return response
+    
+    """
+     -Function to import student details from csv
+    """
+    @action(detail=False, methods=['post'], url_path='import-csv')
+    def import_csv(self,request):
+        file = request.FILES.get('file')
+        if not file:
+            return Response({"error": "CSV file is required"}, status=status.HTTP_400_BAD_REQUEST)
+        decoded_file = TextIOWrapper(file, encoding='utf-8')
+        reader = csv.DictReader(decoded_file)
+        success_count = 0
+        failed = []
+        for idx, row in enumerate(reader,start=1):
+            required_fields = ['username', 'email', 'phone', 'roll_no', 'grade', 'dob', 'admission_date', 'status', 'assigned_teacher']
+            missing = [f for f in required_fields if not row.get(f)]
+            if missing:
+                failed.append({"row":idx, "error":f"missing fields: {','.join(missing)}"})
+            
+            try:
+                teacher = Teacher.objects.get(id=row['assigned_teacher'])
+            except Teacher.DoesNotExist:
+                failed.append({"row":idx,"error":"invalid teacher"})
+                continue
+
+            row_data = {
+            "username": row["username"],
+            "email": row["email"],
+            "role": "student",
+            "first_name": row.get("first_name", ""),
+            "last_name": row.get("last_name", ""),
+            "phone": row["phone"],
+            "roll_no": row["roll_no"],
+            "grade": row["grade"],
+            "dob": row["dob"],
+            "admission_date": row["admission_date"],
+            "status": row["status"],
+            "assigned_teacher": teacher.id,
+            "password": row.get("password", "defaultpass123"),
+            "password2": row.get("password", "defaultpass123"),
+            }
+
+            serializer = RegistrationSerializer(data=row_data, context={"request": request})
+            if serializer.is_valid():
+                try:
+                    serializer.save()
+                    success_count +=1
+                except Exception as e:
+                    failed.append({"row":idx,'error':str(e)})
+            else:
+                failed.append({"row":idx,"error":serializer.errors})
+        
+        return Response({
+                "sucessfully_created":success_count,
+                "failed_entries": failed
+            })     
+            
